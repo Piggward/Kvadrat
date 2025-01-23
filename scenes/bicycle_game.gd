494ac04f47386cfa324dfd30754d616c5c4129ff
Piggward <Axel.stromberg@hotmail.com>
2025-01-23 22:11:04 +0100

    bicycle

--- scenes/bicycle_game.gd
+++ scenes/bicycle_game.gd
@@ -1,14 +1,21 @@
 extends Node2D
 @onready var static_body_2d: StaticBody2D = $StaticBody2D
 @onready var character_body_2d: CharacterBody2D = $CharacterBody2D
-
+@onready var obstacle_spawn = $ObstacleSpawn
+var ospawn_offset: float
+var camera_offset: float
+@onready var camera_2d = $Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	ospawn_offset = character_body_2d.position.y - obstacle_spawn.position.y
+	camera_offset = character_body_2d.position.y - camera_2d.position.y
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	static_body_2d.global_position.y = character_body_2d.global_position.y
+	obstacle_spawn.global_position.y = character_body_2d.global_position.y - ospawn_offset
+	camera_2d.global_position.y = character_body_2d.global_position.y - camera_offset
 	pass
--- scripts/bicycle.gd
+++ scripts/bicycle.gd
@@ -1,35 +1,69 @@
 extends CharacterBody2D
 
 
-const MAXSPEED = 300.0
-const PEDAL_TIME = 0.4
-const SPEED = 50
-
+const MAXSPEED = 400.0
+const STEPS_TO_MAXSPEED = 10.0
+const SPEED = 100
+const MAX_SLOW_DOWN_TIME = 3.0
+#
 var currentPedal = null
-var currentPedalTime = 0.0
+var currentStartSpeed = null
 
+var st = 0.0
+var slow_vel = null
+var idlePedalTime = 0.0
+var currentSlowDownTime = 0.0
+var currentPedalSteps = 0.0
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	
-	if direction == 0:
-		velocity.y = move_toward(velocity.y, 0, 5)
 
-	elif direction != currentPedal:
-		currentPedal = direction
-		velocity.y += -SPEED
-	
-	elif direction == currentPedal:
-		velocity.x += direction * 5
-		currentPedalTime += delta
-		
-		#accelTime += delta
-		#velocity.y = clamp(-5 * (1 + accelTime*2), -MAXSPEED, 0) 
-		#if currentPedalTime >= clamp(((1 - accelTime) * PEDALSPEED), MIN_PEDAL_TIME, PEDALSPEED):
-			#print("Switch!")
-			#currentPedal = -direction
-			#currentPedalTime = 0
-
+	if direction == 0 && velocity.y != 0:
+		if idlePedalTime > 0.4:
+			if slow_vel == null:
+				st = 0.0
+				slow_vel = velocity.y
+				currentPedalSteps = 0.0
+				currentSlowDownTime = MAX_SLOW_DOWN_TIME * (abs(slow_vel) / MAXSPEED)
+			slow_down()
+			st += delta
+		else:
+			idlePedalTime += delta
+	else:
+		if direction != currentPedal:
+			if slow_vel != null || currentStartSpeed == null: 
+				print("stop slow")
+				# Stop slowing down
+				currentSlowDownTime = null
+				slow_vel = null
+				# Set start speed
+				currentStartSpeed = velocity.y
+				
+			currentPedalSteps = clamp(currentPedalSteps+1, 0.0, STEPS_TO_MAXSPEED)
+			set_next_pedal(direction)
+			increase_speed()
+		else:
+			velocity.x += 5 * direction
+	print(velocity)
 	move_and_slide()
+	
+func turn(direction):
+	velocity.x += direction * 5
+	
+func increase_speed():	
+	idlePedalTime = 0.0
+	var diff = MAXSPEED - abs(currentStartSpeed)
+	var progress = currentPedalSteps / STEPS_TO_MAXSPEED
+	var ease = ease(progress, -3.3)
+	var result = abs(currentStartSpeed) + (diff * ease)
+
+	velocity.y = -result
+	
+func set_next_pedal(newpedal):
+	currentPedal = newpedal
+	
+func slow_down():
+	var progress = 1 - ease(st / MAX_SLOW_DOWN_TIME, 0.4)
+	velocity.y = slow_vel * progress
