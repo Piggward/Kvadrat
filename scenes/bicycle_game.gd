0b265927d997a2063d70db30d8253700ff166251
Piggward <Axel.stromberg@hotmail.com>
2025-01-26 12:00:06 +0100

    bicycleses

--- scenes/bicycle_game.gd
+++ scenes/bicycle_game.gd
@@ -1,25 +1,34 @@
 extends Node2D
 @onready var static_body_2d: StaticBody2D = $StaticBody2D
-@onready var character_body_2d: CharacterBody2D = $CharacterBody2D
+@onready var player: CharacterBody2D = $CharacterBody2D
 @onready var obstacle_spawn = $ObstacleSpawn
 var ospawn_offset: float
 var camera_offset: float
 @onready var camera_2d = $Camera2D
 @export var distance_to_level: int
+@export var distance_to_obstacle: int
 var current_level = 1
+var next_spawn = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ospawn_offset = character_body_2d.position.y - obstacle_spawn.position.y
-	camera_offset = character_body_2d.position.y - camera_2d.position.y
+	ospawn_offset = player.position.y - obstacle_spawn.position.y
+	camera_offset = player.position.y - camera_2d.position.y
+	next_spawn = player.position.y - distance_to_obstacle
+	obstacle_spawn.amount = 1
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	static_body_2d.global_position.y = character_body_2d.global_position.y
-	obstacle_spawn.global_position.y = character_body_2d.global_position.y - ospawn_offset
-	camera_2d.global_position.y = character_body_2d.global_position.y - camera_offset
-	if (character_body_2d.position.y <= (distance_to_level * current_level) * -1):
+	static_body_2d.global_position.y = player.global_position.y
+	obstacle_spawn.global_position.y = player.global_position.y - ospawn_offset
+	camera_2d.global_position.y = player.global_position.y - camera_offset
+	if (player.position.y <= (distance_to_level * current_level) * -1):
 		current_level += 1
 		print(current_level)
+		
+	if player.position.y <= next_spawn:
+		obstacle_spawn.spawn_obstacle(Vector2(current_level, 1))
+		next_spawn = player.position.y - distance_to_obstacle
+	
--- scenes/obstacle_spawn.gd
+++ scenes/obstacle_spawn.gd
@@ -2,25 +2,23 @@ extends Area2D
 
 const COLLISION_OBJECT = preload("res://scenes/collision_object.tscn")
 var size:int
+var amount: int
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	size = self.collision_shape_2d.get_shape().get_rect().size.x / 2
-	spawn_obstacle()
 	pass # Replace with function body.
 
-func spawn_obstacle():
-	for i in get_parent().current_level:
+func spawn_obstacle(scale):
+	for i in amount:
 		var obs = COLLISION_OBJECT.instantiate()
 		var rand_pos = -1 if randi_range(0, 1) == 0 else 1 
 		var random_x = randi_range(0, size)
 		var ex = random_x * rand_pos
 		obs.global_position = Vector2(self.global_position.x + ex, self.global_position.y)
-		obs.scale = Vector2(0.5, 0.5)
-		get_parent().get_parent().add_child(obs)
-	await get_tree().create_timer(1).timeout
-	spawn_obstacle()
+		obs.scale = scale
+		get_parent().add_child(obs)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
